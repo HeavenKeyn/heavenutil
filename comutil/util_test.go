59787bec469e6f83cc1858b7891be17c816336c9
HeavenKeyn <34742596+HeavenKeyn@users.noreply.github.com>
2021-10-26 11:07:31 +0800

    Value测试

--- comutil/util_test.go
+++ comutil/util_test.go
@@ -2,9 +2,19 @@ package comutil
 
 import "testing"
 
-func TestValue(t *testing.T) {
+func TestValueToFloat64(t *testing.T) {
+	t.Log(ValueToFloat64(11.0))
 	t.Log(ValueToFloat64(11))
+	t.Log(ValueToFloat64("11"))
+	t.Log(ValueToFloat64("11.0"))
+	t.Log(ValueToFloat64(""))
+}
+
+func TestValueToInt64(t *testing.T) {
+	t.Log(ValueToInt64("11"))
 	t.Log(ValueToInt64(11.0))
+	t.Log(ValueToInt64("11.0"))
+	t.Log(ValueToInt64(""))
 }
 
 func TestHumpToUnderline(t *testing.T) {
